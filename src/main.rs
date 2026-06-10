baf6001fc1941629894432fe08d039766f3d18f2
Yujonpradhananga <yujonpradhan123@gmail.com>
2026-06-10 22:26:23 +0545

    hello world type c

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,13 @@
 use ratatui::{DefaultTerminal, Frame};
+mod timer;
+use timer::timer;
 
-fn main() -> color_eyre::Result<()> {
-    color_eyre::install()?;
+fn main() -> std::io::Result<()> {
     ratatui::run(app)?;
     Ok(())
 }
 
 fn app(terminal: &mut DefaultTerminal) -> std::io::Result<()> {
-    loop {
-        terminal.draw(render)?;
-        if crossterm::event::read()?.is_key_press() {
-            break Ok(());
-        }
-    }
-}
-
-fn render(frame: &mut Frame) {
-    frame.render_widget("hello world", frame.area());
+    timer();
+    Ok(())
 }
---
+++ src/timer.rs
@@ -0,0 +1,9 @@
+use crossterm::event;
+use ratatui::layout::{Constraint, Layout, Rect};
+use ratatui::style::{Style, Stylize};
+use ratatui::text::{Line, Span};
+use ratatui::{DefaultTerminal, Frame};
+
+pub fn timer() {
+    print!("niga")
+}
