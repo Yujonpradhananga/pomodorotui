3b2f493a9cc0fe98ae4fcf91855b4ae5670f1157
Yujonpradhananga <yujonpradhan123@gmail.com>
2026-06-13 00:49:18 +0545

    timer finally

--- src/timer.rs
+++ src/timer.rs
@@ -1,13 +1,15 @@
-use crossterm::{event, terminal};
-use ratatui::layout::{Constraint, Layout, Rect};
-use ratatui::style::{Style, Stylize};
-use ratatui::text::{Line, Span};
 use ratatui::{DefaultTerminal, Frame};
 use std::thread::sleep;
 use std::time::{Duration, Instant};
 
 pub fn timer(terminal: &mut DefaultTerminal) {
     let now = Instant::now();
-    sleep(Duration::new(2, 0));
-    println!("{}", now.elapsed().as_secs());
+    sleep(Duration::new(0, 0));
+    let i: &mut i32 = &mut 0;
+    let time: &mut i32 = &mut 1500;
+    while i <= time {
+        println!("{i}");
+        sleep(Duration::new(1, 0));
+        terminal.clear();
+    }
 }
